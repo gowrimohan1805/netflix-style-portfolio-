413e8dab0aee36af0a8da675f82fc07f7cc6fbc2
gowrimohan1805 <gowrishankar1852002@gmail.com>
2026-01-13 13:50:10 +0530

    Fix router basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ProjectDetail from './pages/ProjectDetail';
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
